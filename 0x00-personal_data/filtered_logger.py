41c9926a1da710d41bf3a283035fe5d8ba2f1635
Alaa Okair <alaaokair330@gmail.com>
2024-08-30 02:50:46 +0000

    filter logger

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""Module for Personal Data"""
+from typing import List
+import re
+import logging
+from os import environ
+import mysql.connector
+
+
+FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """Function to hide personal data"""
+    for field in fields:
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+
+    def format(self, record: logging.LogRecord) -> str:
+        """the format method"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+        
+        
+def get_logger() -> logging.Logger:
+    """ Returns Logger"""
+
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(list(FIELDS)))
+    logger.addHandler(handler)
+
+    return logger
